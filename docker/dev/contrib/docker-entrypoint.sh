7b7012889975d2399895c81b8886e270d40812c8
CJ Virtucio <cjv287@gmail.com>
2022-01-27 16:33:31 -0500

    added mvp for dev.sh (TODO: see if group permissions of files can be maintained instead of just using gid 0

--- docker/dev/contrib/docker-entrypoint.sh
+++ docker/dev/contrib/docker-entrypoint.sh
@@ -3,6 +3,12 @@
 set -e
 
 function main {
+  printf "%s:x:%d:%d:Developer:%s:/usr/sbin/nologin" \
+    "${USER}" \
+    "$(id -u)" \
+    0 \
+    "${HOME}" >> /etc/passwd
+
   >&2 echo "dropping into shell"
   exec /bin/bash
 }
